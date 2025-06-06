f0dedeed36e95b99e1508b2b6906a86ad417ae7d
yasi6969 <santiagocn08@gmail.com>
2025-06-05 22:30:53 -0500

    fix theme

--- scripts/tema.js
+++ scripts/tema.js
@@ -3,8 +3,8 @@
     const themes = {
         dark: {p: '#1F1F1F', b: '#121212', t1: '#fff', t2: '#ddd', br: '#444', btn: '#e76f51', f: 1, bgt: '255, 255, 255'},
         nocturno: {p: '#1B263B', b: '#0D1B2A', t1: '#fff', t2: '#ddd', br: '#415A77', btn: '#12bedb', f: 1, bgt: '255, 255, 255'},
-        verde: {p: '#18221C',b: '#0F1612',t1: '#E5FFE5',t2: '#ddd',br: '#3C5143',btn: '#00FF99',f: 1,bgt: '255, 255, 255'},
-        neon: {p: '#1F1F2E',b: '#141422',t1: '#FFFFFF',t2: '#ddd',br: '#4A3A6A',btn: '#CEA5FF',f: 1,bgt: '255, 255, 255'},
+        verde: {p: '#18221C',b: '#0F1612',t1: '#fff',t2: '#ddd',br: '#3C5143',btn: '#00FF99',f: 1,bgt: '255, 255, 255'},
+        neon: {p: '#1F1F2E',b: '#141422',t1: '#fff',t2: '#ddd',br: '#4A3A6A',btn: '#CEA5FF',f: 1,bgt: '255, 255, 255'},
         light: {p: '#FAFAFA', b: '#F0F0F0', t1: '#111', t2: '#222', br: '#bbb', btn: '#FF3B3B', f: 2, bgt: '0, 0, 0'},
     };
 
