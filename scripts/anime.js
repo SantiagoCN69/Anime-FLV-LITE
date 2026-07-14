3c08929badc1c1cd470fbef482a2dbdc705eb272
SANTIAGOCN69 <SANTIAGOCN08@GMAIL.COM>
2026-07-13 21:55:26 -0500

    update ui

--- scripts/anime.js
+++ scripts/anime.js
@@ -401,6 +401,7 @@ const createEpisodeButton = (ep, vistos = [], internalId) => {
   img.className = 'episode-thumb';
   img.src = `https://cdn.animeav1.com/screenshots/${internalId}/${ep.number}.jpg`;
   img.alt = `Episodio ${ep.number}`;
+    img.loading = 'lazy';
   }
   else {
     img.className = 'episode-thumb no-thumb';
