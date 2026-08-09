51cabdb7bc82d971559c17ccf80c608189fd2f5b
SANTIAGOCN69 <SANTIAGOCN08@GMAIL.COM>
2026-08-09 14:33:46 -0500

    update version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = 'v8.3.0';
+const CACHE_VERSION = 'v8.4.0';
 
 const STATIC_CACHE = `anizenlite-static-${CACHE_VERSION}`;
 const PAGE_CACHE = `anizenlite-pages-${CACHE_VERSION}`;
