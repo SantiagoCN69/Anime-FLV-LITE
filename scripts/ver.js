8c1b415ee776d70ba7ab139451b5facadfc8cb07
SANTIAGOCN69 <SANTIAGOCN08@GMAIL.COM>
2026-09-12 12:29:52 -0500

    fix styles no cargan servidores

--- scripts/ver.js
+++ scripts/ver.js
@@ -935,7 +935,7 @@ async function cargarEpisodios() {
     console.error("Error al cargar episodios desde API:", err);
     const controles = document.getElementById("controles");
     if (controles) {
-      controles.innerHTML = "<span class='span-carga'><h2>Error</h2><br>No se pudieron cargar los episodios.</span>";
+      controles.innerHTML = "<span class='span-carga'>404 No se pudieron cargar los episodios.</span>";
     }
     throw err;
   }
